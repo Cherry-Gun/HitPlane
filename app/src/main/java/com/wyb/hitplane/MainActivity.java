720aacea62a604be1ad9d813307427962e0e8c03
jason <jason-white@live.cn>
2016-03-27 00:39:30 +0800

    创建版本库，开始敲代码

---
+++ app/src/main/java/com/wyb/hitplane/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wyb.hitplane;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
